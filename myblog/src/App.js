91a9fef74af6d55c6339812497ea74325fb82c83
edumatheus65 <ematheusneves13@gmail.com>
2023-03-27 12:29:25 -0300

    Create API using fetch

--- myblog/src/App.js
+++ myblog/src/App.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+
+  const [blogs, setBlogs] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setBlogs(data))
+  }, [])
   
     return (
       <div className="App">
+        <header className='App-header'>
+          <h1>all blogs</h1>
+          {
+            blogs && blogs.map(blog => (
+              <div key={blog.id}>{blog.title}</div>
+            ))
+          }
+        </header>
         
       </div>
     );
